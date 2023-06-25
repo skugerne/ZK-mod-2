ce64cdd249c3b11c10b39da29f042e9cc79b8704
skugerne <31828871+skugerne@users.noreply.github.com>
2023-06-25 15:34:27 +0200

    Add the basics.

---
+++ skuggMod2.sdd/modinfo.lua
@@ -0,0 +1,13 @@
+return {
+	name = 'Another Skugg Zero-K Mod',
+	description = 'A mod to explore ideas and screw around',
+	shortname = 'skuggmod2',
+	version = 'v1',
+	mutator = '1',
+	game = '',
+	shortGame = '',
+	modtype = 1,
+	depend = {
+		[[rapid://zk:stable]]
+	},
+}
