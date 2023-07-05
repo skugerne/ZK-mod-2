44da9283860d3e116b1a504e23a9f8a0c5b203ca
skugerne <31828871+skugerne@users.noreply.github.com>
2023-07-06 00:42:59 +0200

    New version to upload.

--- skuggMod2.sdd/modinfo.lua
+++ skuggMod2.sdd/modinfo.lua
@@ -2,7 +2,7 @@ return {
 	name = 'Skugg Supercomm Mod (dev)',   -- remove dev before packaging to sdz
 	description = 'A mod to explore time-limited but otherwise unbalanced commander upgrades.',
 	shortname = 'skugg-s-c-mod-dev',      -- remove dev before packaging to sdz
-	version = 'v7',                       -- increment sometimes (can not overwrite something already uploaded)
+	version = 'v8',                       -- increment sometimes (can not overwrite something already uploaded)
 	mutator = '1',
 	game = '',
 	shortGame = '',
